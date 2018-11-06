0b9a827f8a35164ca78bf9e859290cfb2298b4c4
Eric Kristiansen <eric.kristiansen@mail.com>
2018-11-06 13:10:49 -0800

    Some testing - need to refactor for better testing

--- WorldsGreatestBankingLedger/ConsoleLedger/Testing.cs
+++ WorldsGreatestBankingLedger/ConsoleLedger/Testing.cs
@@ -1,28 +1,61 @@
-﻿using ConsoleLedger.Controllers;
+﻿
+using ConsoleLedger.Controllers;
+using Models;
 using NUnit.Framework;
+using Models.Services;
 
 namespace ConsoleLedger
 {
     [TestFixture]
     class Testing
     {
+        private const string StrTest = "test";
+
         [TestCase]
         public void VerifyCredentials()
         {
             var lc = new LoginController();
-            lc.SeedUser("test", "test");
+            lc.SeedUser(StrTest, StrTest);
 
-            Assert.AreEqual(lc.VerifyCredentials("test", "test"), true);
-            Assert.AreNotEqual(lc.VerifyCredentials("test", "Test"), true);
-            Assert.AreNotEqual(lc.VerifyCredentials("Test", "test"), true);
+            Assert.AreEqual(lc.VerifyCredentials(StrTest, StrTest), true);
+            Assert.AreNotEqual(lc.VerifyCredentials(StrTest, "Test"), true);
+            Assert.AreNotEqual(lc.VerifyCredentials("Test", StrTest), true);
+            Assert.AreNotEqual(lc.VerifyCredentials(StrTest, string.Empty), true);
+            Assert.AreNotEqual(lc.VerifyCredentials(string.Empty, StrTest), true);
         }
 
+        [TestCase]
+        public void CurrentUser()
+        {
+            var lc = new LoginController();
+            lc.SeedUser(StrTest, StrTest);
+            lc.VerifyCredentials(StrTest, StrTest);
 
-        //remove user
+            Assert.AreEqual(lc.GetCurrentUser().UserName.Equals(StrTest), true);
 
-        //add account
+        }
 
-        //remove account
+        [TestCase]
+        public void CreateHistory()
+        {
+            var lc = new LoginController();
+            lc.SeedUser(StrTest, StrTest);
+            lc.VerifyCredentials(StrTest, StrTest);
+
+            var am = new AccountModel(new DataService());
+            Assert.AreNotEqual(am.GetHistory(), null);
+        }
+
+        [TestCase]
+        public void GetBalance()
+        {
+            var lc = new LoginController();
+            lc.SeedUser(StrTest, StrTest);
+            lc.VerifyCredentials(StrTest, StrTest);
+
+            var am = new AccountModel(new DataService());
+            Assert.AreNotEqual(am.Balance(), null);
+        }
 
     }
 }
